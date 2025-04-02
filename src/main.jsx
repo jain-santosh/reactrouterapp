3c98fc25e90e1803fe410336e09b128a697c311a
Santosh Jain <santoshjain.id@gmail.com>
2025-04-03 00:48:47 +0530

    react-router tutorial step 9

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
+import ErrorPage from './error-apge';
+import Contact from './routes/contact';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactsId",
+        element: <Contact />,
+      },
+    ]
+  },
+  
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1>Hello world</h1>
+    <RouterProvider router={router}/>
   </StrictMode>,
 );
